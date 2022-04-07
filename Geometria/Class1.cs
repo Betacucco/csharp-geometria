3015816880b435c496393c415ceb067dcda62c38
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-07 17:02:19 +0200

    Commenti

--- Geometria/Class1.cs
+++ Geometria/Class1.cs
@@ -8,17 +8,19 @@ namespace CalcoliGeometrici
 {
     internal class Geometria
     {
+        //ATTRIBUTI
         private int baseRettangolo;
         private int altezzaRettangolo;
         private int areaRettangolo;
         private int perimetroRettangolo;
 
+        //COSTRUTTORE CON BASE E ALTEZZA
         public Geometria(int baseRettangolo, int altezzaRettangolo)
         {
             this.baseRettangolo = baseRettangolo;
             this.altezzaRettangolo = altezzaRettangolo;
         }
-
+        //COSTRUTTORE CON BASE, ALTEZZA, AREA E PERIMETRO
         public Geometria(int baseRettangolo, int altezzaRettangolo, int areaRettangolo, int perimetroRettangolo)
         {
             this.baseRettangolo = baseRettangolo;
@@ -26,19 +28,19 @@ namespace CalcoliGeometrici
             this.areaRettangolo = areaRettangolo;
             this.perimetroRettangolo = perimetroRettangolo;
         }
-
+        //METODO PER IL CALCOLO DELL'AREA
         public int CalcolaArea(int baseRettangolo, int altezzaRettangolo)
         {
             int areaRettangolo = baseRettangolo * altezzaRettangolo;
             return areaRettangolo;
         }
-
+        //METODO PER IL CALCOLO DEL PERIMETRO
         public int CalcolaPerimetro(int baseRettangolo, int altezzaRettangolo)
         {
             int perimetroRettangolo = (2 * baseRettangolo) + (2 * altezzaRettangolo);
             return perimetroRettangolo;
         }
-
+        //METODO PER STAMPARE LE VARIE MISURE DEL RETTANGOLO E IL DISEGNO
         public void StampaRettangolo()
         {
             Console.WriteLine("----------RETTANGOLO----------");
@@ -48,7 +50,7 @@ namespace CalcoliGeometrici
             Console.WriteLine("Area: " + perimetroRettangolo + "cm^2");
             Disegna(baseRettangolo,altezzaRettangolo);
         }
-
+        //METODO CHE DISEGNA IL RETTANGOLO
         public void Disegna(int baseRettangolo, int altezzaRettangolo)
         {
             this.baseRettangolo = baseRettangolo;

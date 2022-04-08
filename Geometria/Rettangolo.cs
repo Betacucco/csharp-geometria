9a9bfc01250e03217e49976e80370cc14963f69a
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-08 09:53:11 +0200

    MIgliorie

--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -4,56 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalcoliGeometrici
+namespace Geometria
 {
-    internal class Geometria
+    internal class Rettangolo
     {
         //ATTRIBUTI
         private int baseRettangolo;
         private int altezzaRettangolo;
-        private int areaRettangolo;
-        private int perimetroRettangolo;
 
         //COSTRUTTORE CON BASE E ALTEZZA
-        public Geometria(int baseRettangolo, int altezzaRettangolo)
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
         {
             this.baseRettangolo = baseRettangolo;
             this.altezzaRettangolo = altezzaRettangolo;
         }
-        //COSTRUTTORE CON BASE, ALTEZZA, AREA E PERIMETRO
-        public Geometria(int baseRettangolo, int altezzaRettangolo, int areaRettangolo, int perimetroRettangolo)
-        {
-            this.baseRettangolo = baseRettangolo;
-            this.altezzaRettangolo = altezzaRettangolo;
-            this.areaRettangolo = areaRettangolo;
-            this.perimetroRettangolo = perimetroRettangolo;
-        }
+
         //METODO PER IL CALCOLO DELL'AREA
-        public int CalcolaArea(int baseRettangolo, int altezzaRettangolo)
+        public int CalcolaArea()
         {
             int areaRettangolo = baseRettangolo * altezzaRettangolo;
             return areaRettangolo;
         }
+
         //METODO PER IL CALCOLO DEL PERIMETRO
-        public int CalcolaPerimetro(int baseRettangolo, int altezzaRettangolo)
+        public int CalcolaPerimetro()
         {
             int perimetroRettangolo = (2 * baseRettangolo) + (2 * altezzaRettangolo);
             return perimetroRettangolo;
         }
+
         //METODO PER STAMPARE LE VARIE MISURE DEL RETTANGOLO E IL DISEGNO
         public void StampaRettangolo()
         {
             Console.WriteLine("Base: " + baseRettangolo + "cm");
             Console.WriteLine("Altezza: " + altezzaRettangolo + "cm");
-            Console.WriteLine("Perimetro: " + areaRettangolo + "cm");
-            Console.WriteLine("Area: " + perimetroRettangolo + "cm^2");
-            Disegna(baseRettangolo,altezzaRettangolo);
+            Console.WriteLine("Perimetro: " + CalcolaPerimetro() + "cm");
+            Console.WriteLine("Area: " + CalcolaArea() + "cm^2");
+            Disegna();
         }
+
         //METODO CHE DISEGNA IL RETTANGOLO
-        public void Disegna(int baseRettangolo, int altezzaRettangolo)
+        public void Disegna()
         {
-            this.baseRettangolo = baseRettangolo;
-            this.altezzaRettangolo = altezzaRettangolo;
 
             for(int i = 0; i < baseRettangolo; i++)
             {
